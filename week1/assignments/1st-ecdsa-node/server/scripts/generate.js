e4e2d644fabe2b266ad4869a33bea488f6dc894c
thecodingliva <xlr8nur@protonmail.com>
2024-05-28 14:38:12 +0300

    add ecdsa node

---
+++ week1/assignments/1st-ecdsa-node/server/scripts/generate.js
@@ -0,0 +1,9 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography(utils");
+const privateKey = secp.utils.randomPrivateKey()
+
+console.log("privateKey", toHex(privateKey))
+
+const publicKey = secp.getPublicKey(privateKey);
+
+console.log("publicKey", toHex(publicKey));
